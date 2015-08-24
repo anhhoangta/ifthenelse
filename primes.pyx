6a4a43591520e565d6fa7cd37c6a1d7c92e53f18
anhhoangta <anhhoangta@gmail.com>
2015-08-24 11:40:03 +0700

    Update

--- primes.pyx
+++ primes.pyx
@@ -2,16 +2,23 @@ __author__ = 'anhhoangta'
 import time
 import math
 
-count = 0
+primeinLineCount = 0
+primeCount = 0
 time_start = time.clock()
-for i in range (2,1000):
+i=2
+while primeCount < 100:
     for j in range(2,int(math.sqrt(i))+1):
         if i%j == 0:
             break
     else:
-        print(i)
-        count += 1
-        if count >100:
-            break
+        print("{0:5d}" .format(i), end="")
+        primeCount += 1
+        primeinLineCount += 1
+        if primeinLineCount > 9:
+            print("")
+            primeinLineCount = 0
+    i += 1
+
 time_end = time.clock()
+print()
 print("Print first 100 primes number in: {0:5f} miliseconds" .format((time_end-time_start)*1000))
