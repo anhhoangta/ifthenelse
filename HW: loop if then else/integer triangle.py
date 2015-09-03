417e9c10aef7c096cbbcdda03af40f6948a73b72
anhhoangta <anhhoangta@gmail.com>
2015-09-03 20:08:30 +0700

    update

--- HW: loop if then else/integer triangle.py
+++ HW: loop if then else/integer triangle.py
@@ -1,7 +1,10 @@
-while True:
-    number = int(input("Enter a integer: "))
-
-    for i in range(1, number+1):
-        for j in range(1, i+1):
-            print(j, end=" ")
-        print()
+oddnum = int(input("Please input an odd integer: "))
+listNumber =[]
+if oddnum % 2 == 0:
+    print("Sorry, the number you entered is an even number")
+else:
+    for i in reversed(range(1, oddnum+1)):
+        if i % 2 != 0:
+            listNumber = [i]*i
+            print(str(''.join(map(str, listNumber))).center(oddnum*len(str(oddnum))))
+    print()
