94513a7bc4a11366888f194a72956ed728033e60
anhhoangta <anhhoangta@gmail.com>
2015-09-05 08:30:09 +0700

    List

--- Set Programming/List.py
+++ Set Programming/List.py
@@ -1,7 +1,7 @@
 def filterDup(list):
     for item in list:
         while list.count(item) > 1:
-            del list[list.index(item)]
+            list.remove(item)         #also use: del list[list.index(item)]
     return list
 
 print(filterDup(["tom", "jerry", "dog", "tom"]))
