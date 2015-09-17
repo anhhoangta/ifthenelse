4434678dd2f42f857982eda6ce1dae6fa9007eba
anhhoangta <anhhoangta@gmail.com>
2015-09-17 10:52:57 +0700

    update

--- Built-in Functions/enumerate.py
+++ Built-in Functions/enumerate.py
@@ -1,5 +1,5 @@
 x = [1, 5, 7, 3, 8]
 
 print("x = {0}" .format(x))
-for i in range(len(x)):
-    print("x[{0:d}] = {1:d}" .format(i, x[i]))
+for xx, xy in enumerate(x):
+    print("x[%d] = %d" %(xx, xy))
